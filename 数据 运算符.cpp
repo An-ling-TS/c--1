72905a92548abb30f40c3484b94539e2a990e279
Rain <32219252+lianganqing@users.noreply.github.com>
2017-11-03 22:09:43 +0800

    Add files via upload

---
+++ 数据 运算符.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	//实型数据舍入误差 
+	float a,b;
+	a=123456.789e5;
+	b=a+20;
+	printf("%f\n",a);
+	printf("%f\n",b);
+	float c;
+	double d;
+	c=33333.33333;
+	d=33333.333333333333333;
+	printf("%f\n%f\n",c,d);
+	int fuction(int e);
+	return 0;
+ } 
+
+
